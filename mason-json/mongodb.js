967290e1393333348f8251bf62cea5fcb898cd48
benmag <benmag@users.noreply.github.com>
2019-04-07 23:35:34 +1000

    Fix indentation

--- mason-json/mongodb.js
+++ mason-json/mongodb.js
@@ -1,9 +1,9 @@
 module.exports = {
-    name: 'mongodb',
-    image: 'mongo',
-    type: 'service',
-    volumes: {
-        './storage/data/mongodb':'/data/db'
-    },
-    ports: ["27017:27017"],
+  name: 'mongodb',
+  image: 'mongo',
+  type: 'service',
+  volumes: {
+    './storage/data/mongodb':'/data/db'
+  },
+  ports: ["27017:27017"],
 }
--- mason-json/nodejs.js
+++ mason-json/nodejs.js
@@ -1,12 +1,12 @@
 module.exports = {
-    name: 'web', 
-    build: '.',
-    base: 'codemasonhq/nodejs', 
-    type: 'service',
-    volumes: {
-        './':'/app',
-    },
-    ports: [
-        "3000:3000",
-    ],
+  name: 'web', 
+  build: '.',
+  base: 'codemasonhq/nodejs', 
+  type: 'service',
+  volumes: {
+    './':'/app',
+  },
+  ports: [
+    "3000:3000",
+  ],
 }
